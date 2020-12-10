1e6ba149e15a59f7be8963ea565bf8e488a45327
Stefanos Angelidis <sangelid@livy.inf.ed.ac.uk>
2020-12-10 13:45:29 +0000

    train-spm.py cmd arguments

--- src/utils/train-spm.py
+++ src/utils/train-spm.py
@@ -3,16 +3,16 @@
 import sys
 from data import spm_train_from_json
 
+if len(sys.argv) < 3 or sys.argv[1][:2] == '-h':
+    print('usage: python3 train-spm <json_file> <prefix> [<max_sentences>]')
+    sys.exit()
+
+fname_tmp = './spm.tmp'
 fname_in = sys.argv[1]
 prefix = sys.argv[2]
 if len(sys.argv) > 3:
-    fname_tmp = sys.argv[3]
-    if len(sys.argv) > 4:
-        max_sents = int(sys.argv[4])
-    else:
-        max_sents = None
+    max_sents = int(sys.argv[3])
 else:
-    fname_tmp = './spm.tmp'
     max_sents = None
 
 spm_train_from_json(fname_in, prefix, fname_tmp=fname_tmp, max_sents=max_sents)
